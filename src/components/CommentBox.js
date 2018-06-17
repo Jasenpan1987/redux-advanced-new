83a449b43982946a87b8dbf05840aefba5471947
Jasenpan1987 <jasenpan@gmail.com>
2018-06-17 15:08:05 +1000

    wire up hoc and pass down the props

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { saveComment, fetchComments } from "actions";
+import requireAuth from "components/requireAuth";
 
 class CommentBox extends Component {
   state = {
@@ -51,4 +52,4 @@ class CommentBox extends Component {
 export default connect(
   null,
   { saveComment, fetchComments }
-)(CommentBox);
+)(requireAuth(CommentBox));
