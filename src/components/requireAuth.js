3bc50963155ed914e78cd1cbbbf23b0ab5db6685
Jasenpan1987 <jasenpan@gmail.com>
2018-06-17 14:56:17 +1000

    step2: create the boilerplate hoc

---
+++ src/components/requireAuth.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default ChildComponent => {
+  class ComposedComponent extends Component {
+    render() {
+      return <ChildComponent />;
+    }
+  }
+
+  return ComposedComponent;
+};
